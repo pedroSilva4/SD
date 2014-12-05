229dff06b26f8380919e7909d0a5d8ad222a0d86
pedroSilva4 <a64345@alunos.uminho.pt>
2014-12-05 15:46:58 +0000

    testing.

--- trabalho/WareHouse_server/test/warehouse/Test_tasks_tools.java
+++ trabalho/WareHouse_server/test/warehouse/Test_tasks_tools.java
@@ -7,6 +7,9 @@
 package warehouse;
 
 import com.warehouse.server.WareHouse;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,10 +20,11 @@ public class Test_tasks_tools {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         
         final WareHouse wh = new WareHouse();
-        class suplly extends Thread
+        
+        class supply extends Thread
         {
            
            @Override
@@ -30,12 +34,54 @@ public class Test_tasks_tools {
                {
                    wh.add_tool("tool"+i, 5, true);
                }
+               System.out.println("supply done");
            }
         }
         
         class definer extends Thread{
-           
+           public void run()
+           {
+               HashMap<String,Integer> tools = new HashMap<>();
+               
+               tools.put("tool0",2);
+               tools.put("tool4",2);
+               tools.put("tool6",2);
+               
+               wh.define_task("Escacar Pedra", tools);
+           }
         }
+           
+         class worker extends Thread{
+             public void run(){
+                 while(true){
+                     try {
+                         wh.task_request("Escacar Pedra");
+                         System.out.println("toua realizar tasks");
+                         sleep(4000);
+                         wh.task_return("Escacar Pedra");
+                         
+                     } catch (InterruptedException ex) {
+                         ex.printStackTrace();
+                     }
+                 }
+             }
+         }
+         
+        
+         supply sp  = new supply();
+         sp.start();
+         sp.join();
+         
+         definer df = new definer();
+         df.start();
+         df.join();
+         worker[] wks = new worker[5];
+         
+         
+         for(int i = 0;i<5;i++){
+         wks[i] = new worker();
+         wks[i].start();    
+         }
         // TODO code application logic here
     }
     
