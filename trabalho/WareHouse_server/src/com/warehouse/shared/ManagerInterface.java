0f225e9ca745200fe289b8d63b1416455273e154
pedroSilva4 <a64345@alunos.uminho.pt>
2014-12-26 17:10:10 +0000

    asdasd

--- trabalho/WareHouse_server/src/com/warehouse/shared/ManagerInterface.java
+++ trabalho/WareHouse_server/src/com/warehouse/shared/ManagerInterface.java
@@ -6,11 +6,25 @@
 
 package com.warehouse.shared;
 
+import com.warehouse.tasks.Task;
+import com.warehouse.tasks.TaskType;
+import com.warehouse.util.TaskNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  *
  * @author Pedro
  */
 public interface ManagerInterface {
-    
+     
+     public void define_task(String name,HashMap<String,Integer> tools);
+     public HashMap<String,TaskType> get_taskTypes();
+     public TaskType get_taskType(String name);
+     public int task_request(String taskType,String user) throws InterruptedException;
+     public void task_return(int task_id) throws TaskNotFoundException;
+     public void add_tool(String name,int qtt,boolean ret);
+     public int waiton(int[] tasks) throws InterruptedException;
+     public List<Task> getActiveTasks();
     
 }
